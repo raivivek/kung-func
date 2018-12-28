b972e326ad7657184879687153ef548534cf6f39
Vivek Rai <mail@raivivek.in>
2018-12-28 04:37:31 +0000

    Update 2.1-ex.rkt

--- sicp/ch-2/2.1-ex.rkt
+++ sicp/ch-2/2.1-ex.rkt
@@ -1,10 +1,10 @@
-#lang planet neil/sicp
+#lang sicp
 
 (define (make-rat n d)
   (let ((g (gcd n d)))
     (cond ((and (> n 0) (< d 0)) (cons (- (/ n g)) (/ d g)))
           ((and (< n 0) (> d 0)) (cons (/ n g) (/ d g)))
-          ((and (< n 0) (< d 0)) (cons (- (/ n g) (- (/ d g)))))
+          ((and (< n 0) (< d 0)) (cons (/ (- n) g) (/ (- d) g)))
           (else (cons (/ n g) (/ d g))))))
 
 (define (numer x) (car x))
@@ -14,4 +14,14 @@
   (newline)
   (display (numer x))
   (display "/")
-  (display (denom x)))
+(display (denom x)))
+
+;; TEST
+(print-rat (make-rat 1 2))
+; 1/2
+
+(print-rat (make-rat 1 -2))
+; -1/-2
+
+(print-rat (make-rat -1 -2))
+; 1/2
